4549df360c148342a923e2e57e611cbf5294c454
jyyblue <jyyblue1987@outlook.com>
2017-01-17 09:59:53 +0800

    [fixed] search option in list page.

--- app/scripts/controllers/list.js
+++ app/scripts/controllers/list.js
@@ -13,23 +13,41 @@ angular.module('starwarApp')
     $scope.hint_message ='Use the form to search for <br> "People" or "Movies"';
 
 	$scope.search = '';
-
+	$scope.search_mode = 'People';
+	// search_mode == 'People' ? 'e.g. Chewbacca, Yoda, Boba Fett : 'e.g. Empire Strikes Back, The Force Awakens'
     $scope.item_list = [];
 
 	$scope.onSearch = function() {		
 		$scope.item_list = [];
 	    $scope.loading = true;
-	    var url = 'http://swapi.co/api/people/?search='+ $scope.search;
+	    var url = '';
+
+	    if( $scope.search_mode == 'People' )
+			url = 'http://swapi.co/api/people/?search='+ $scope.search;
+		else
+			url = 'http://swapi.co/api/films/?search='+ $scope.search;
+
 	    swapi.get(url)
-		  .then(function(peoples) {
+		  .then(function(list) {
 		  	$scope.loading = false;
-		    $scope.item_list = peoples.results;
+		    $scope.item_list = list.results;
 		  });	
 	}  
 
 	$scope.detail = function(item) {
-		$state.go('detail', { item: JSON.stringify(item) });
+		if( $scope.search_mode == 'People' )
+			$state.go('detail', { item: JSON.stringify(item) });
+		else
+			$state.go('movie', { item: JSON.stringify(item) });
+
 	}
+
+	$scope.$watch('search_mode', function(newValue, oldValue) {
+        if( newValue == oldValue )
+            return;
+
+        $scope.item_list = [];
+    });
 
 	
   });
